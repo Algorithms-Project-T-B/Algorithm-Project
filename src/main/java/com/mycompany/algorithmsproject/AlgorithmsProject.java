81fb0cd93d4a9866855f19a47384fd8cdc8ec19d
ttasneem17 <qurainitasneem@gmail.com>
2025-12-10 19:26:01 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/algorithmsproject/AlgorithmsProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algorithmsproject;
+
+/**
+ *
+ * @author Tasneem
+ */
+public class ALgorithmsProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
